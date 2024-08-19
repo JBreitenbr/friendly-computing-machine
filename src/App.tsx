8578bca5bf0dbfc4056a67c4a6fd8e3027f38919
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-08-19 14:04:47 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1 className="text-3l">Friendly Computing Machine</h1>
+      <h3 className="bg-teal-500">Der Name ist Programm</h3>
+      <p className="bg-slate-700 text-white">Und Tailwind läuft auch!</p>
+    </main>
+  )
+}

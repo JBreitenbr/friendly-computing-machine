aa13a4145657df876464e2c6515941cf3492e32a
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-08-19 14:13:40 +0000

    added Mode classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css'
 export default function App() {
   return (
     <main>
-      <h1 className="text-3l">Friendly Computing Machine</h1>
-      <h3 className="bg-teal-500">Der Name ist Programm</h3>
-      <p className="bg-slate-700 text-white">Und Tailwind läuft auch!</p>
+      <h1 className="text-3xl">Friendly Computing Machine</h1>
+      <h3 className="bg-teal-500 text-xl text-center">Der Name ist Programm</h3>
+      <p className="bg-slate-700 text-white text-center">Und Tailwind läuft auch!</p>
     </main>
   )
 }
